74607d48e386731a235f7d133b908d53110dcaee
Martin Kavalar <mk@katercalling.com>
2019-12-12 14:43:21 +0100

    Initial commit parsing first Clojure expressions

---
+++ README.md
@@ -0,0 +1,6 @@
+# lezer-clojure
+
+This is a Clojure grammar for the
+[lezer](https://lezer.codemirror.net/) parser system.
+
+The code is licensed under an MIT license.
---
+++ test/test-clojure.js
@@ -0,0 +1,15 @@
+const {parser} = require("../dist/index")
+const {fileTests} = require("lezer-generator/dist/test.js")
+
+let fs = require("fs"), path = require("path")
+let caseDir = __dirname
+
+for (let file of fs.readdirSync(caseDir)) {
+  if (!/\.txt$/.test(file)) continue
+
+  let name = /^[^\.]*/.exec(file)[0]
+  describe(name, () => {
+    for (let {name, run} of fileTests(fs.readFileSync(path.join(caseDir, file), "utf8"), file))
+      it(name, () => run(parser))
+  })
+}
