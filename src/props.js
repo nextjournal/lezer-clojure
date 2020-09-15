223c7920c9df0c4385272bf8c08ed43a36a7b0fc
Matthew Huebert <mhuebert@gmail.com>
2020-09-15 14:21:18 +0200

    add node props

---
+++ src/props.js
@@ -0,0 +1,6 @@
+const {NodeProp} = require("lezer-tree")
+
+export const prefixColl = NodeProp.flag()
+export const coll = NodeProp.flag()
+export const prefixEdge = NodeProp.flag()
+export const sameEdge = NodeProp.flag()
