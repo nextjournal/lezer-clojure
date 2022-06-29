137484d30a6becff0fc2b581f5b3e2e0ca7d9a2b
Andrea Amantini <lo.zampino@gmail.com>
2022-06-29 11:53:02 +0200

    Create distinct Props

--- src/props.js
+++ src/props.js
@@ -1,10 +1,10 @@
 import {NodeProp} from "@lezer/common"
 
-// FIXME: naïvely restored to previoius NodeProp.flag() behaviour
-let flag = new NodeProp({deserialize: str => true})
+// TODO: naïvely restored to previoius NodeProp.flag() behaviour. Can we do any better?
+let flag = () => new NodeProp({deserialize: str => true})
 
-export const coll = flag
-export const prefixColl = flag
-export const prefixEdge = flag
-export const sameEdge = flag
-export const prefixContainer = flag
+export const coll = flag()
+export const prefixColl = flag()
+export const prefixEdge = flag()
+export const sameEdge = flag()
+export const prefixContainer = flag()
